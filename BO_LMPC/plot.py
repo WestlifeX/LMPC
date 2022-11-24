93e9022e75b0b417d0c7227c6b41b433bc7fb246
WestlifeX <1165996282@qq.com>
2022-11-24 19:19:57 +0800

    update

--- BO_LMPC/plot.py
+++ BO_LMPC/plot.py
@@ -25,15 +25,17 @@ all = np.zeros((N_alg, end))
 all_best = np.zeros((N_alg, end))
 bo_data = np.zeros((N, end))
 tlbo_data = np.zeros((N, end))
-tlbo_all_data = np.zeros((N, end))
+direct_data = np.zeros((N, end))
+unlim_data = np.zeros((N, end))
 idxs = [1, 2, 3, 4, 5, 6]
 
 for i in range(N):
     idx = idxs[i]
     data = []
     for name in file_names:
-        if name == 'robust_bo_{}.md'.format(idx) or name == 'robust_tvbo_{}.md'.format(idx) or name == 'robust_own.md'\
-                or name == 'robust_tvbo_all_{}.md'.format(idx):
+        if name == 'robust_bo_{}.md'.format(idx) or name == 'robust_tvbo_{}.md'.format(idx) or \
+                name == 'robust_tvbo_direct_{}.md'.format(idx) \
+                or name == 'robust_tvbo_unlim_{}.md'.format(idx):
             with open('./new_results/' + name) as f:
                 lines = f.readlines()
                 lines = [float(i.strip().strip('[[').strip(']]')) for i in lines]
@@ -56,13 +58,14 @@ for i in range(N):
                     all[1, :] += lines[:end] / N
                     all_best[1, :] += current_best[:end] / N
                     tlbo_data[i - 1, :] = current_best[:end]
-                elif name == 'robust_tvbo_all_{}.md'.format(idx):
-                    all[3, :] += lines[:end] / N
-                    all_best[3, :] += current_best[:end] / N
-                    tlbo_all_data[i - 1, :] = current_best[:end]
-                else:
+                elif name == 'robust_tvbo_direct_{}.md'.format(idx):
                     all[2, :] += lines[:end] / N
                     all_best[2, :] += current_best[:end] / N
+                    direct_data[i - 1, :] = current_best[:end]
+                else:
+                    all[3, :] += lines[:end] / N
+                    all_best[3, :] += current_best[:end] / N
+                    unlim_data[i - 1, :] = current_best[:end]
 
     #         plt.plot(lines[0:50], label=name.strip('.md'))
     #         plt.plot(np.linspace(1, 50, 50), np.ones(50)*y)
@@ -70,23 +73,29 @@ for i in range(N):
     # plt.show()
 bo_std = np.std(bo_data, axis=0)
 tlbo_std = np.std(tlbo_data, axis=0)
-tlbo_all_std = np.std(tlbo_all_data, axis=0)
+direct_std = np.std(direct_data, axis=0)
+unlim_std = np.std(unlim_data, axis=0)
 # plt.plot(all[0])
 # plt.plot(all[1])
 # plt.plot(all[2])
 # plt.show()
 colors = ['steelblue', 'deeppink']
 x = np.linspace(1, 30, 30)
-plt.plot(x, all_best[0], label='generic bo', lw=2, color=colors[0])
+plt.plot(x, all_best[0], label='Generic BO', lw=2, color=colors[0])
 # plt.step(x, all_best[0], label='generic bo', lw=2, color='purple')
 # plt.errorbar(np.arange(all_best.shape[1]), all_best[0], bo_std, capsize=3)
-plt.fill_between(range(1, all_best.shape[1]+1), all_best[0]-bo_std, all_best[0]+bo_std, alpha=0.3, color=colors[0])
-plt.plot(x, all_best[1], label='transfer learning bo', lw=2, color=colors[1])
+plt.fill_between(range(1, all_best.shape[0]+1), all_best[0]-bo_std, all_best[0]+bo_std, alpha=0.3, color=colors[0])
+plt.plot(x, all_best[1], label='Efficient BO', lw=2, color=colors[1])
 # plt.step(x, all_best[1], label='transfer learning bo', lw=2, color='slateblue')
 # plt.errorbar(np.arange(all_best.shape[1]), all_best[1], bo_std, capsize=3)
 plt.fill_between(range(1, all_best.shape[1]+1), all_best[1]-tlbo_std, all_best[1]+tlbo_std, alpha=0.3, color=colors[1])
 # plt.plot(all_best[2], label='lmpc')
 
+plt.plot(x, all_best[2], label='BO', lw=2, colors=colors[2])
+plt.fill_between(range(1, all_best.shape[2]+1), all_best[2]-direct_std, all_best[2]+direct_std, alpha=0.3, colors=colors[2])
+
+plt.plot(x, all_best[3], label='Unbounded Efficient-BO', lw=2, colors=colors[3])
+plt.fill_between(range(1, all_best.shape[3]+1), all_best[2]-unlim_std)
 # plt.plot(all_best[3], label='tvbo all')
 # plt.errorbar(np.arange(all_best.shape[1]), all_best[1], bo_std, capsize=3)
 # plt.fill_between(range(all_best.shape[1]), all_best[3]-tlbo_all_std, all_best[3]+tlbo_all_std, alpha=0.3)
