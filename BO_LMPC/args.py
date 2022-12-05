e91e2557ebfd2c068d64d851ad70157088530370
WestlifeX <1165996282@qq.com>
2022-12-05 18:16:17 +0800

    update

--- BO_LMPC/args.py
+++ BO_LMPC/args.py
@@ -13,13 +13,9 @@ Bd = np.array([[0.], [1.]])
 # R = np.eye(1)  # np.array([[1]]) 非线性下真实的R
 A = np.vstack((np.hstack((Ad, Bd)), np.hstack((np.zeros((Bd.shape[1], Ad.shape[1])), np.eye(Bd.shape[1])))))
 B = np.vstack((Bd, np.eye(Bd.shape[1])))
-<<<<<<< HEAD
 x0 = [4., 1.]
-=======
-x0 = [-6, -0.6]
->>>>>>> 29931cf41752ed03c8653bef4114a2007c032d90
 coef = 0.04
-totalIterations = 50
+totalIterations = 30
 def compute_uncertainty(xt):
     return [np.clip(np.sign(xt[0]) * (np.exp(xt[0] ** 2 / 10) - 1), -0.03, 0.03),
         np.clip(np.sign(xt[1]) * (-np.exp(xt[1] ** 2 / 10) + 1), -0.03, 0.03)]
